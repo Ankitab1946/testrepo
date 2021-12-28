d364d45c7dd7b01cbb7e21ca305cadcab6997c60
Ankitab1946 <96777490+Ankitab1946@users.noreply.github.com>
2021-12-28 18:07:13 +0530

    Add files via upload
    uploaded already created file

---
+++ FirstScript.py
@@ -0,0 +1,15 @@
+print('Hello World')
+x = int(98.6)
+print(x)
+
+# The code below for input user name and welcome them
+
+name = input("Enter your name")
+print('Hello',name)
+
+# Inpur Hours and Output calculation rate per hour
+
+hrs = input("Enter Hours:")
+rph = input("Rate Per Hours:")
+trph = float(hrs) * float(rph)
+print('Pay:',trph)
